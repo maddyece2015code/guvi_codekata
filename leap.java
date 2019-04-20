92b509b493cfffe360ca795803000b8a4819e742
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-04-20 17:32:52 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class leap{
+public static void main(String args[]){
+ int num;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter the year");
+ num=sc.nextInt();
+ if(num%4==0){
+    System.out.println("YES"); 
+  }
+ else{
+    System.out.println("NO"); 
+  }
+}
+}
