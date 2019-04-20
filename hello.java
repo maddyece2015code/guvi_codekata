d992053566a74658d2e8b6bace0652c15533e637
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-04-20 17:49:59 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hello{
+public static void main(String args[]){
+int num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+for(int i=1;i<=num;i++)
+ System.out.println("Hello");
+}
+}
