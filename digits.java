6624d36d1f8d2bcf705757b5c3ca144bb7fc5310
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-04-25 16:13:11 +0530

    Add files via upload

---
+++ digits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class digits{
+public static void main(String args[]){
+int count=0,num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+while(num!=0)
+ {
+  num=num/10;
+  count++;
+ }
+System.out.println(count);
+}
+}
