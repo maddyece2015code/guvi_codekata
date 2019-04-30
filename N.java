29edb6cb9e945316556ca57b852fad7e66c13012
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-04-30 15:24:58 +0530

    Add files via upload

---
+++ N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class N{
+public static void main(String args[])
+{
+ int n,res=0;
+ Scanner sc=new Scanner(System.in);
+ n=sc.nextInt();
+ for(int i=1;i<=5;i++)
+  {
+   res=n*i;
+   System.out.print(res+" ");
+  }
+}
+}
