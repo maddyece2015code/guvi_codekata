1f1a36d50e6118d9ad5eab82e1cc1c18ce872432
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-01 09:52:22 +0530

    Update hunt3.java

--- hunt3.java
+++ hunt3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class hunt3{
 public static void main(String arg[])
 {
-int n;
+int n,flag=0,b;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
 int a[]=new int[n];
@@ -14,8 +14,14 @@ for(int i=0;i<n;i++)
 {
  if(a[i]==i)
  {
+   flag=1;
    System.out.print(a[i]+" ");
  }
 }
+ if(flag==0)
+ {
+  b=-1;
+  System.out.print(b);
+ }
 }
 }
