51a2b72a4a9945c099afa69b241e983ec12bcc3a
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-07 11:42:49 +0530

    Add files via upload

---
+++ B23.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+class B23 {
+    public static void main(String args[])
+    {
+        int N,temp;
+        Scanner sc=new Scanner (System.in);
+        N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<N;i++)
+        {
+            for(int j=i+1;j<N;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(arr[0]);
+    }
+    
+}
+
