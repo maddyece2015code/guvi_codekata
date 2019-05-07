9af1a0e03fb088720ad0f2ed44c6e2e21c875146
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-07 11:26:13 +0530

    Add files via upload

---
+++ B21.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B21 {
+    public static void main(String args[])
+    {
+        float N,A,D,S;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        A=sc.nextInt();
+        D=sc.nextInt();
+        S= (N / 2) * (2 * A + (N - 1) * D); 
+        int r=Math.round(S);
+        System.out.println(r);
+    }
+    
+}
