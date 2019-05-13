8051bc15ab97c69b2ddd52033371600e720f4b79
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-13 20:59:10 +0530

    Add files via upload

---
+++ B35.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B35 {
+    public static void main(String args[])
+    {
+        String str;
+        int count=0;
+        Scanner sc=new Scanner (System.in);
+        str=sc.nextLine();
+        for(int i=0;i<str.length();i++)
+        {
+          if(Character.isDigit(str.charAt(i)))
+          {
+            count++;   
+          }
+        }
+        System.out.println(count);
+        }
+    }
