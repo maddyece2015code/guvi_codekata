b4d35052cac0ddcc6351a0102e29ac23c78c42b7
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-13 19:20:21 +0530

    Add files via upload

---
+++ B31.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B31 {
+    public static void main(String args[])
+    {
+        String str;
+        Scanner sc=new Scanner (System.in);
+        str=sc.nextLine();
+        String s=str.replace(" ", "");
+        int len=s.length();
+        System.out.println(len);
+        }
+    }
+    
