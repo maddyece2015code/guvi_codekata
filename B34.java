fa489d4c977c1f74c715987f819b40ee5a128849
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-13 19:58:37 +0530

    Add files via upload

---
+++ B34.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B34 {
+    public static void main(String args[])
+    {
+        String str;
+        int count=0;
+        Scanner sc=new Scanner (System.in);
+        str=sc.nextLine();
+        String s[]=str.split("[.]");
+        int len=s.length;
+        System.out.println(len);
+        }
+    }
