c49cbd89c8cc846c43479c8af6c469ddf9eea053
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-13 19:36:33 +0530

    Add files via upload

---
+++ B33.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B33 {
+    public static void main(String args[])
+    {
+        String str;
+        Scanner sc=new Scanner (System.in);
+        str=sc.nextLine();
+        String s[]=str.split(" ");
+        int len=s.length;
+        System.out.println(len-1);
+        }
+    }
