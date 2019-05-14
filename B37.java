7ee731f8952298548f04b4e5d7955591b942bc23
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-14 11:06:59 +0530

    Add files via upload

---
+++ B37.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+    class B37 {
+    public static void main(String args[])
+    {
+        int n1,n2,temp;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        temp=n1;
+        n1=n2;
+        n2=temp;
+        System.out.println(n1+" "+n2);
+    }
+    
+}
