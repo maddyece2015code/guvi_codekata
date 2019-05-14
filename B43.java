a3f53bd536e437df5f2a35ddbb9851c53a860ff2
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-14 18:22:21 +0530

    Add files via upload

---
+++ B43.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B43 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int l1,l2;
+        String S;
+        S=sc.nextLine();
+        String str[]=S.split(" ");
+        System.out.println(str[0]+str[1]);
+    }
+}
