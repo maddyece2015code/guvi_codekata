02a60fa853f8f845fe5fc75572563590ac0aaabc
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-14 17:52:22 +0530

    Add files via upload

---
+++ B41.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B41 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int K;
+        String S;
+        K=sc.nextInt();
+        S=sc.next();
+        for(int i=1;i<=K;i++)
+        {
+            System.out.println(K);
+        }
+    }
+    
+    
+}
