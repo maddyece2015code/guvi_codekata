0c79f9b6bd5f187ab3b49a65fbcbd1269a7dc078
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-14 12:36:21 +0530

    Update B40.java

--- B40.java
+++ B40.java
@@ -15,15 +15,22 @@ public class B40 {
         int f1=1,f2=1,f3,n;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        System.out.print(f1+" ");
-        System.out.print(f2+" ");
-        for(int i=0;i<n-2;i++)
+        if(n==1)
         {
+         System.out.print(f1+" ");
+        }
+        else
+        {
+         System.out.print(f2+" ");   
+         System.out.print(f2+" ");
+         for(int i=0;i<n-2;i++)
+         {
             f3=f1+f2;
             f1=f2;
             f2=f3;
             System.out.print(f3+" ");
-        }
+         }
+        }   
     }
     
 }
