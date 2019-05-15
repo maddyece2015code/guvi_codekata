fcdf2845fde5fae83eac3c3cd14be97c52b1617d
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-15 11:32:33 +0530

    Add files via upload

---
+++ B46.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B46 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int N;
+        N=sc.nextInt();
+        System.out.println(N+1);
+    }
+}
