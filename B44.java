58aee3ec0ef428df5dce5d9404f9f2a634732805
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-05-15 11:14:35 +0530

    Add files via upload

---
+++ B44.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class B44 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n>=1&&n<=10)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
