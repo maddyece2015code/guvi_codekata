f137192e003b833dd6689c32ee89090cecbc7026
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-06-12 11:00:28 +0530

    Add files via upload

---
+++ Hunt9.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class Hunt9 {
+    public static void main(String args[])
+    {
+        int n,zero=1,f=0,s=0,sum;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                sum=Math.abs(arr[i]+arr[j]);
+                if(sum<=zero)
+                {
+                    zero=sum;
+                    f=arr[i];
+                    s=arr[j];
+                }
+                 if(zero==-1||zero==+1)
+                    {
+                        break;
+                    }
+                    
+            }
+        }
+        System.out.println(f+" "+s);
+    }
+}
