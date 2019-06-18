d301275449fe4ee505766983847c26b4ddecd210
maddyece2015code <36659210+maddyece2015code@users.noreply.github.com>
2019-06-18 19:40:19 +0530

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playerguvi;
+
+/**
+ *
+ * @author Madhavan
+ */
+import java.util.*;
+public class P7 {
+    public static void main(String args[])
+    {
+        String s;
+        String res="";
+        Scanner sc=new Scanner(System.in);
+        s=sc.next();
+        char temp;
+        char arr[]=s.toCharArray();
+        for(int i=0;i<arr.length;i=i+2)
+        {
+           temp=arr[i];
+           arr[i]=arr[i+1];
+           arr[i+1]=temp;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            res+=arr[i];
+        }
+        System.out.println(res);
+    }
+}
